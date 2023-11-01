e9341710b7d7132d954d7568569764503405d7a5
alain le moal <wolfdu28@aol.com>
2023-11-01 16:30:10 +0100

    si image affiche pokemon si pas image affiche ???

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 /*import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import viteLogo from "/vite.svg";*/
 import "./App.css";
-import MyTitle from "./components/MyTitle"*/
+/*import MyTitle from "./components/MyTitle"*/
 import PokemonCard from'./components/PokemonCard';
 
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,38 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
+const PokemonCard = () => {
+    const pokemon = pokemonList [1];
+    return (
+    <figure>
+    {pokemon.imgSrc ? <img src={pokemon.imgSrc} />:<p>???</p>}
+ 
+    <figcaption> 
+        {pokemon.name}
+    </figcaption>
+     </figure>
+    )
+};
 
-function PokemonCard(){
+
+
+
+/*function PokemonCard(){
  return <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"/>
 <figcaption> 
     Bulbasaur
 </figcaption>
 </figure>
-}
+}*/
 
 
 export default PokemonCard;
